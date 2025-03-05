28682b0262428046fa6c23b2b1a47f070e7cceba
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-03-04 20:09:33 -0500

    almost done

---
+++ main.py
@@ -0,0 +1,13 @@
+names_and_bid_dic = {}
+
+again='yes'
+while(again=="yes"):
+    name = input("What is your name: ")
+    bid = int(input("What is your bid: $"))
+    names_and_bid_dic[name] = bid
+    again = input("Are there any other bidders? Type 'yes' or 'no': ")
+    if again=='yes':
+        print('\n'*100)
+#todo - find the largest bidder and print it to the screeen
+for key in names_and_bid_dic:
+    if names_and_bid_dic[key]>
